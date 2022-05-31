aee17d72b1c9189570a751b2c83233255e4249fd
Alexey Gerasev <alexey.gerasev@gmail.com>
2022-05-31 11:37:51 +0700

    Fix considered functions

--- src/asm.rs
+++ src/asm.rs
@@ -1,9 +1,9 @@
-use crate::{Consumer, Producer};
+use crate::{Consumer, HeapRb, Producer};
 
-pub fn push(prod: &mut Producer<i32>, x: i32) -> Result<(), i32> {
+pub fn push(prod: &mut Producer<i32, &HeapRb<i32>>, x: i32) -> Result<(), i32> {
     prod.push(x)
 }
 
-pub fn pop(cons: &mut Consumer<i32>) -> Option<i32> {
+pub fn pop(cons: &mut Consumer<i32, &HeapRb<i32>>) -> Option<i32> {
     cons.pop()
 }
