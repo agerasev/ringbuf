83f6fa6ff8d03c685fbcaa7e644e8e2ec47b48c1
Alexey Gerasev <alexey.gerasev@gmail.com>
2023-03-17 13:56:56 +0700

    Test overwrite for LocalRb

--- src/tests/overwrite.rs
+++ src/tests/overwrite.rs
@@ -1,5 +1,6 @@
-use crate::{HeapRb, Rb};
+use crate::{HeapRb, LocalRb, Rb};
 use alloc::vec::Vec;
+use core::mem::MaybeUninit;
 
 #[test]
 fn push() {
@@ -26,4 +27,17 @@ fn push_slice() {
     let mut rb = HeapRb::<i32>::new(2);
     rb.push_slice_overwrite(&[0, 1, 2, 3, 4, 5]);
     assert_eq!(rb.pop_iter().collect::<Vec<_>>(), [4, 5]);
+}
+
+#[test]
+fn push_local() {
+    let mut rb = LocalRb::<i32, [MaybeUninit<i32>; 2]>::default();
+
+    assert_eq!(rb.push_overwrite(0), None);
+    assert_eq!(rb.push_overwrite(1), None);
+    assert_eq!(rb.push_overwrite(2), Some(0));
+
+    assert_eq!(rb.pop(), Some(1));
+    assert_eq!(rb.pop(), Some(2));
+    assert_eq!(rb.pop(), None);
 }
