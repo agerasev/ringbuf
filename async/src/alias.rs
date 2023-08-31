a502ab4c0bf4903526bb340017a64f463c32d7ac
Alexey Gerasev <alexey.gerasev@gmail.com>
2023-08-31 08:13:18 +0700

    Small fix, bump versions

--- async/src/alias.rs
+++ async/src/alias.rs
@@ -24,7 +24,7 @@ impl<T> AsyncHeapRb<T> {
 
 pub type AsyncStaticRb<T, const N: usize> = AsyncRb<Static<T, N>>;
 pub type AsyncStaticProd<'a, T, const N: usize> = AsyncProd<&'a AsyncStaticRb<T, N>>;
-pub type AsyncStaticCons<'a, T, const N: usize> = AsyncProd<&'a AsyncStaticRb<T, N>>;
+pub type AsyncStaticCons<'a, T, const N: usize> = AsyncCons<&'a AsyncStaticRb<T, N>>;
 
 impl<T, const N: usize> Default for AsyncRb<Static<T, N>> {
     fn default() -> Self {
